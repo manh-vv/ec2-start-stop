dca3fa0c7413c1dac54f9b6a5906b2715212c43f
manh <manh.vv.htth@gmail.com>
2023-06-30 04:53:13 +0000

    feat: add wait for instance status

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
-# ec2-start-stop
+# ec2 start stop
+
 Start and Stop AWS EC2 Instances from another always active Instance
 
 Use it for server development / test / stage that should not always be running.
 
-1. Install [aws-cli](https://aws.amazon.com/it/cli/) into another EC2 instance that is always running 24 hours on 24, write a script like this [ec2-start-stop.sh](https://github.com/vpistis/ec2-start-stop/blob/master/ec2-start-stop.sh) provided.
-2. Put the script into cron (crontab -e) on this instance always active, for example using the [cron.txt](https://github.com/vpistis/ec2-start-stop/blob/master/cron.txt) provided.
+1. Install [aws-cli](https://aws.amazon.com/it/cli/) into another EC2 instance that is always running 24 hours on 24, write a script like this [ec2cmd.sh](./ec2cmd.sh) provided.
+2. Put the script into cron (crontab -e) on this instance always active, for example using the [cron.txt](./cron.txt) provided.
 
 **ATTENTION**: always make a backup and test first using it!
--- ec2cmd.sh
+++ ec2cmd.sh
@@ -8,13 +8,17 @@
 # https://github.com/vpistis/ec2-start-stop
 
 AWS_CLI=/usr/local/bin/aws
+
+PROFILE=lecle-prod
+
 # Array degli id delle istance da gestire, esempio INSTANCE_IDS=(i-12345678 i-00000000 i-11111111)
-INSTANCE_IDS=(i-12345678 i-00000000)
+INSTANCE_IDS=(i-040b61fe7d9fd1ed6)
+
 # Array degli Elastic IP da collegare alle istanze, rispettare l'ordine dell'array INSTANCE_IDS.
 # Esempio: PUBBLIC_IP=(123.456.789.000 000.222.444.122).
 #
 # L'ip nella poosizione 0 viene collegato all'id della posizione 0.
-PUBBLIC_IPS=(123.456.789.000)
+PUBBLIC_IPS=()
 AWS_CLI_LOG=~/aws-cli.log
 DRY_RUN=
 
@@ -28,7 +32,8 @@ echo " -dry commands aggiungono l'opzione --dry-run, ovvero non eseguono veramen
 stop-instances()
 {
 echo stop-instances - START DATE: `date` >> $AWS_CLI_LOG
-$AWS_CLI ec2 stop-instances $DRY_RUN --instance-ids ${INSTANCE_IDS[@]} >> $AWS_CLI_LOG 2>&1
+$AWS_CLI ec2 --profile $PROFILE stop-instances $DRY_RUN --instance-ids ${INSTANCE_IDS[@]} >> $AWS_CLI_LOG 2>&1
+$AWS_CLI ec2 --profile $PROFILE wait instance-stopped --instance-ids ${INSTANCE_IDS[@]} >> $AWS_CLI_LOG 2>&1
 echo stop-instances - END DATE: `date` >> $AWS_CLI_LOG
 #aws ec2 stop-instances --instance-ids=$INSTANCE_IDS
 }
@@ -36,7 +41,8 @@ echo stop-instances - END DATE: `date` >> $AWS_CLI_LOG
 start-instances()
 {
 echo start-instances - START DATE: `date` >> aws-cli.log
-$AWS_CLI ec2 start-instances $DRY_RUN --instance-ids ${INSTANCE_IDS[@]} >> $AWS_CLI_LOG 2>&1
+$AWS_CLI ec2 --profile $PROFILE start-instances $DRY_RUN --instance-ids ${INSTANCE_IDS[@]} >> $AWS_CLI_LOG 2>&1
+$AWS_CLI ec2 --profile $PROFILE wait instance-status-ok --instance-ids ${INSTANCE_IDS[@]} >> $AWS_CLI_LOG 2>&1
 echo start-instances - END DATE: `date` >> aws-cli.log
 }
 
@@ -46,7 +52,7 @@ echo associate-eip - START DATE: `date` >> aws-cli.log
 sleep 20
 count=0
 for ip in ${PUBBLIC_IPS[@]}; do
-$AWS_CLI ec2 associate-address $DRY_RUN --instance-id=${INSTANCE_IDS[$count]} --public-ip=${ip} >> $AWS_CLI_LOG 2>&1
+$AWS_CLI ec2 --profile $PROFILE associate-address $DRY_RUN --instance-id=${INSTANCE_IDS[$count]} --public-ip=${ip} >> $AWS_CLI_LOG 2>&1
 count=$(( $count + 1 ))
 done
 echo associate-eip - END DATE: `date` >> aws-cli.log
